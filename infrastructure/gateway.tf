c141710dbdc63e1ed974911445cfe843d9d8d128
Rodion Chachura <geekrodion@gmail.com>
2019-10-12 18:09:06 +0300

    update: conditionally create route53-related resources

--- infrastructure/gateway.tf
+++ infrastructure/gateway.tf
@@ -2,23 +2,6 @@ resource "aws_api_gateway_rest_api" "api" {
   name = "tf-${var.name}"
 }
 
-resource "aws_api_gateway_domain_name" "api" {
-  certificate_arn = "${var.certificate_arn}"
-  domain_name     = "${var.domain}"
-}
-
-resource "aws_route53_record" "api" {
-  name    = "${aws_api_gateway_domain_name.api.domain_name}"
-  type    = "A"
-  zone_id = "${var.zone_id}"
-
-  alias {
-    evaluate_target_health = true
-    name                   = "${aws_api_gateway_domain_name.api.cloudfront_domain_name}"
-    zone_id                = "${aws_api_gateway_domain_name.api.cloudfront_zone_id}"
-  }
-}
-
 resource "aws_api_gateway_method" "api_root" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
   resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
@@ -79,8 +62,30 @@ resource "aws_api_gateway_deployment" "api" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "${var.name}"
 }
+
+# Only if the domain variable specified these resources will be created.
 resource "aws_api_gateway_base_path_mapping" "api" {
+  count = "${var.domain != "" ? 1 : 0}"
   api_id      = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "${aws_api_gateway_deployment.api.stage_name}"
   domain_name = "${aws_api_gateway_domain_name.api.domain_name}"
+}
+
+resource "aws_api_gateway_domain_name" "api" {
+  count = "${var.domain != "" ? 1 : 0}"
+  certificate_arn = "${var.certificate_arn}"
+  domain_name     = "${var.domain}"
+}
+
+resource "aws_route53_record" "api" {
+  count = "${var.domain != "" ? 1 : 0}"
+  name    = "${aws_api_gateway_domain_name.api.domain_name}"
+  type    = "A"
+  zone_id = "${var.zone_id}"
+
+  alias {
+    evaluate_target_health = true
+    name                   = "${aws_api_gateway_domain_name.api.cloudfront_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.api.cloudfront_zone_id}"
+  }
 }
